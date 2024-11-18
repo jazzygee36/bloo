66763f11e990590acec948abab10ff17a1cf79ae
Samson Mesioye <jazzygee36gmail.com>
2024-11-18 23:26:25 +0100

    enhancement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import './globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 const queryClient = new QueryClient();
 
 export default function RootLayout({
@@ -17,7 +17,6 @@ export default function RootLayout({
       >
         <QueryClientProvider client={queryClient}>
           {children}
-          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </body>
     </html>
